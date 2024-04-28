6f1b41065f5090f85d435fbab3978519618d4e21
Paulo Fernando <paulofernandodcm16@gmail.com>
2024-04-28 17:14:20 -0300

    11

--- src/main/java/DeployRender.java
+++ src/main/java/DeployRender.java
@@ -1,13 +1,11 @@
-package com.br.abracepetz.project;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AbracePetzApplication {
+public class DeployRender {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AbracePetzApplication.class, args);
+		SpringApplication.run(DeployRender.class, args);
 	}
 
 }
